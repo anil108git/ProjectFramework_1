4b2042977efdcbc543b446a25f5404a85cc0267f
Anil <ak108.mail@gmail.com>
2026-06-11 15:42:22 +0530

    methods added

--- LearningJS/practiceDay2.js
+++ LearningJS/practiceDay2.js
@@ -25,3 +25,48 @@ const arrSize = 5;
 console.log(missingnumber(arr, arrSize));
 
 
+//Reverse a string
+
+function reverseString() {
+    const str = 'anil kumar';
+    const newstr1 = str.split('').reverse().join('');
+    console.log(newstr1);
+    let newstr2 = '';
+    for(let i =str.length-1; i>=0; i--) {
+        newstr2 += str[i];
+    }
+    console.log(newstr2);
+}
+reverseString();
+
+
+// longest word in the string
+function longestword(str) {
+    const string1 = str.split(' ');
+    let longestword = '';
+    for(let i = 0; i<string1.length; i++) {
+        if(string1[i].length > longestword.length) {
+            longestword = string1[i];
+        }
+    }
+    console.log(longestword);
+
+}
+
+longestword('Check if a string contains only unique characters');
+
+// Remove all whitespace from a string
+function removewhiteSpace() {
+    const text = 'a b e v e g e anil';
+    let newtext = '';
+    for(let i= 0; i< text.length; i++) {
+        if(!text[i].includes(' ')) {
+            newtext += text[i];
+        }
+    }
+    console.log(newtext);
+}
+
+removewhiteSpace();
+
+// Find the longest substring without repeating characters
